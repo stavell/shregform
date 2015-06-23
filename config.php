5064f2293caffd128d9fe164c4b36d871eabd2ba
root <root@stavl.com>
2015-06-23 10:24:16 +0300

    dl;ask :

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('METEO2_DB_HOST', 'stavl.com');
+define('METEO2_DB_HOST', 'localhost');
 define('METEO2_DB_USER', 'shreg');
 define('METEO2_DB_PASS', 'shreg@pass');
 define('METEO2_DB_DB', 'shumenxc_comp');
